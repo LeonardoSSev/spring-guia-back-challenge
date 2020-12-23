25610b601e3127b2c66c7f67949492fcd00b95c2
leonardossev <leonardossev@gmail.com>
2020-12-23 09:02:07 -0300

    fix(project): Corrige nome de pacotes

---
+++ src/test/java/com/leonardossev/guiabackchallenge/GuiabackChallengeApplicationTests.java
@@ -0,0 +1,10 @@
+package com.leonardossev.guiabackchallenge;
+
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
+
+@ExtendWith(SpringExtension.class)
+class GuiabackChallengeApplicationTests {
+
+}
