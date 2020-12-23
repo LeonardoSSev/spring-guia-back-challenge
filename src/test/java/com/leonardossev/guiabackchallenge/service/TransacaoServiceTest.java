b2be8565ff4556147151c6d13ce12c5fb222fc56
leonardossev <leonardossev@gmail.com>
2020-12-23 09:45:18 -0300

    refactor(test): Prepara mock de geracao de transacao

--- src/test/java/com/leonardossev/guiabackchallenge/service/TransacaoServiceTest.java
+++ src/test/java/com/leonardossev/guiabackchallenge/service/TransacaoServiceTest.java
@@ -1,16 +1,28 @@
 package com.leonardossev.guiabackchallenge.service;
 
+import com.leonardossev.guiabackchallenge.model.Transacao;
+import com.leonardossev.guiabackchallenge.model.TransacaoFiltro;
+import com.leonardossev.guiabackchallenge.service.impl.GeradorTransacaoServiceImpl;
+import com.leonardossev.guiabackchallenge.service.impl.TransacaoServiceImpl;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
+import org.mockito.Mock;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
 
 import java.security.InvalidParameterException;
+import java.sql.Timestamp;
+import java.time.LocalDateTime;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+import static org.mockito.Mockito.when;
 
 @ExtendWith(SpringExtension.class)
 public class TransacaoServiceTest {
+
+    @Mock
+    private GeradorTransacaoServiceImpl geradorTransacaoService;
 
     @InjectMocks
     private TransacaoServiceImpl transacaoService;
@@ -21,7 +33,9 @@ public class TransacaoServiceTest {
 
     @Test
     public void deveRetornarListaCujoTamanhoIgualAoMesMultiplicadoPeloPrimeiroDigitoDoId() {
-        var transacaoFiltro = new TransacaoFiltro(1, 12, 2020);
+        var transacaoFiltro = new TransacaoFiltro(1000, 1, 2020);
+
+        this.prepararGeracaoTransacao(transacaoFiltro);
 
         var transacaoLista = this.transacaoService.listarTransacao(transacaoFiltro);
 
@@ -62,6 +76,14 @@ public class TransacaoServiceTest {
         var transacaoFiltro = new TransacaoFiltro(999, 13, 2020);
 
         assertThrows(InvalidParameterException.class, () -> this.transacaoService.listarTransacao(transacaoFiltro));
+    }
+
+    private void prepararGeracaoTransacao(TransacaoFiltro transacaoFiltro) {
+        var descricao = "Lorem ipsum";
+        var data = Timestamp.valueOf(LocalDateTime.now());
+        var valor = 10;
+
+        when(this.geradorTransacaoService.gerarTransacao(transacaoFiltro, 1)).thenReturn(new Transacao(descricao, data, valor));
     }
 
 }
