1bf06217252d01ad6cda482065bd643cd12a7bc9
leonardossev <leonardossev@gmail.com>
2020-12-23 02:20:14 -0300

    feature(project): Inicia projeto com Spring Boot

---
+++ src/main/java/com/leonardo/guiabackchallenge/GuiabackChallengeApplication.java
@@ -0,0 +1,13 @@
+package com.leonardossev.guiabackchallenge;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GuiabackChallengeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GuiabackChallengeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/leonardo/guiabackchallenge/GuiabackChallengeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.leonardossev.guiabackchallenge;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GuiabackChallengeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
