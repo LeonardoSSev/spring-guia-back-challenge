273d41a0fd7fbd5c4d6918a6e03e75fcdf86024e
leonardossev <leonardossev@gmail.com>
2020-12-25 20:59:47 -0300

    refactor(gerador): Remove service desnecessario

--- src/main/java/com/leonardossev/guiabackchallenge/service/GeradorTransacaoService.java
+++
@@ -1,9 +0,0 @@
-package com.leonardossev.guiabackchallenge.service;
-
-import com.leonardossev.guiabackchallenge.model.Transacao;
-import com.leonardossev.guiabackchallenge.model.TransacaoFiltro;
-
-public interface GeradorTransacaoService {
-
-    Transacao gerarTransacao(TransacaoFiltro transacaoFiltro, int indiceAtual);
-}
