fc1edad4d94e606413dce913bc10f3cf2dbd5036
leonardossev <leonardossev@gmail.com>
2020-12-25 21:00:30 -0300

    feature(controller): Adiciona endpoint para listagem de transacoes

---
+++ src/main/java/com/leonardossev/guiabackchallenge/controller/TransacaoController.java
@@ -0,0 +1,30 @@
+package com.leonardossev.guiabackchallenge.controller;
+
+import com.leonardossev.guiabackchallenge.model.Transacao;
+import com.leonardossev.guiabackchallenge.model.TransacaoFiltro;
+import com.leonardossev.guiabackchallenge.service.TransacaoService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+@RequestMapping(produces = MediaType.APPLICATION_JSON_VALUE)
+public class TransacaoController {
+
+    @Autowired
+    private TransacaoService transacaoService;
+
+    @GetMapping(value = "/{id}/transacoes/{ano}/{mes}")
+    public ResponseEntity<List<Transacao>> listarTransacao(final TransacaoFiltro transacaoFiltro) {
+        return new ResponseEntity<List<Transacao>>(
+                    this.transacaoService.listarTransacao(transacaoFiltro),
+                    HttpStatus.OK);
+    }
+
+}
